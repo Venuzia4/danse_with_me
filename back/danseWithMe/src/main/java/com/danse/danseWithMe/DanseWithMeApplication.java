f20b36a66621eb2a9b8dc14e50cd977111d755f5
Babongui mabika venuza <vhenuzya@yahoo.fr>
2023-01-05 16:30:38 +0100

    add front

---
+++ back/danseWithMe/src/main/java/com/danse/danseWithMe/DanseWithMeApplication.java
@@ -0,0 +1,13 @@
+package com.danse.danseWithMe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DanseWithMeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DanseWithMeApplication.class, args);
+	}
+
+}
---
+++ back/danseWithMe/src/test/java/com/danse/danseWithMe/DanseWithMeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danse.danseWithMe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DanseWithMeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
