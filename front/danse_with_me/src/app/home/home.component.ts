303cb8c315672e6526c226f934021448dd40398b
loghann <m.loghann@gmail.com>
2023-01-06 10:47:49 +0100

    list of categories ready

--- front/danse_with_me/src/app/home/home.component.ts
+++ front/danse_with_me/src/app/home/home.component.ts
@@ -7,6 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  items = [
+      { label: 'Mot 1', imageUrl: '../../assets/lightyear-frontier.jpg' },
+      { label: 'Mot 2', imageUrl: '../../assets/taipei-101-romain-trystram.jpg' },
+      { label: 'Mot 3', imageUrl: '../../assets/the_tree_shelter_by_ellysiumn.jpg' },
+      { label: 'Mot 4', imageUrl: '../../assets/zelda-botw-marmastry.jpg' }
+    ];
+
   constructor() { }
 
   ngOnInit(): void {
