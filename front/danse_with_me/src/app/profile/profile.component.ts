fe5c979ba61ddbf5ef00e5e06cd7d8056fe0acc9
loghann <m.loghann@gmail.com>
2023-01-13 15:20:27 +0100

    add missed direction to profile from home

--- front/danse_with_me/src/app/profile/profile.component.ts
+++ front/danse_with_me/src/app/profile/profile.component.ts
@@ -2,6 +2,8 @@ import { ActivatedRoute, ParamMap } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 import { User } from '../_models/user';
 import { UserService } from '../_services/user.service';
+import { Dance } from '../_models/dance';
+import { DanceService } from '../_services/dance.service';
 
 @Component({
   selector: 'app-profile',
@@ -9,17 +11,21 @@ import { UserService } from '../_services/user.service';
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
+
+
+  dance!: Dance;
   user!:User;
   id!:string;
 
-  constructor(private userservice:UserService,private route: ActivatedRoute,) {
+  constructor(private userservice:UserService,private route: ActivatedRoute, private danseService: DanceService) {
     this.route.paramMap.subscribe((params:ParamMap)=>{
       return this.userservice.getUser(<string>params.get('id')).subscribe((response)=>{
         console.log(response)
         this.user=response;
       })
 
-    })
+    });
+
    }
 
   ngOnInit(): void {
